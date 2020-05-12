03bdf732af07dfeea28cfe2308414ccc30a3b74f
MEvan774 <57196554+MEvan774@users.noreply.github.com>
2020-05-12 21:32:45 +0200

    Add files via upload

---
+++ 07-AddingVectoren/lib/Vector2d.js
@@ -0,0 +1,21 @@
+/*
+	23-2-2020
+	klasse om een tweedimensionale vector mee aan te geven
+
+	eigenschappen
+	* dx (verschuiving in de x-richting)
+	* dy (verschuiving in de y-richting )
+*/
+
+class Vector2d{
+    constructor(dx,dy){
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    vectorSun(a,b){
+ 	    this.dx = a.dx + b.dx;
+	    this.dy = a.dy + b.dy;
+    }
+ }
+
