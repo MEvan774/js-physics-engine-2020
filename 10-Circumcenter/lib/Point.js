a44dda0b1e5899cce013e397858141c5f0dfe4d2
MEvan774 <57196554+MEvan774@users.noreply.github.com>
2020-06-02 09:11:10 +0200

    Add files via upload
    10-orthocenter upload

---
+++ 10-Circumcenter/lib/Point.js
@@ -0,0 +1,67 @@
+class Point{
+
+  constructor(x,y,radius,color,draggable){
+	this.x = x;
+	this.y = y;
+	this.radius = radius;
+	this.color = color;
+	this.draggable = false || draggable;
+	
+	if(this.draggable){
+	this.drag();
+	}
+  }
+
+      draw(){
+	context.beginPath();
+	context.fillStyle = this.color;
+	//context.fillStyle="rgba(255,0,0,0.4)";
+	context.arc(this.x,this.y,this.radius,0,Math.PI*2);
+	context.stroke();
+	context.fill();
+ }
+
+ get vPos(){
+ 	 return new Vector2d(this.x,this.y);
+ }
+
+ set vPos(vector){
+ 	 this.x = vector.dx;
+	 this.y = vector.dy;
+ }
+
+ drag(){
+		let mouse ={};
+		let distance;
+		let dragging = false;
+
+		addEventListener('mousedown', (evt) => {
+			mouse.x = evt.clientX;
+			mouse.y = evt.clientY;
+
+			let dx = mouse.x - this.x;
+			let dy = mouse.y - this.y;
+			distance = Math.sqrt(dx*dx + dy*dy);
+
+			if(distance < this.radius){
+				dragging = true;
+			}else{
+				dragging = false;
+			}
+
+		})
+		
+		addEventListener('mouseup', () => {
+		dragging = false;
+		})
+
+		addEventListener('mousemove',(evt) => {
+			if(dragging){
+				this.x = evt.clientX;
+				this.y = evt.clientY;
+			}
+		})
+
+}}
+
+
